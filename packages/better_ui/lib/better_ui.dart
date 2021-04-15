c757e39c44c4ba0a76031b4568a24b173fdac573
hhstore <hhglory@outlook.com>
2021-04-15 16:00:56 +0800

    update pkg

--- packages/better_ui/lib/better_ui.dart
+++ packages/better_ui/lib/better_ui.dart
@@ -1,7 +1,4 @@
 library better_ui;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+///
+export 'src/index.dart';
---
+++ packages/better_ui/lib/src/index.dart
@@ -0,0 +1,1 @@
+export 'gesture/gesture.dart';
