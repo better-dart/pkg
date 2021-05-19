e08b8ee7bc091d22e8122d323ee5db1a73bd7e3d
hhstore <hhglory@outlook.com>
2021-05-19 17:26:04 +0800

    update

---
+++ packages/web3/lib/src/client.dart
@@ -0,0 +1,30 @@
+///
+///
+///
+class BetterWalletClient {
+  var _sdkFuse;
+  var _sdkWeb3;
+
+  ////////////////////////////////////////////////////////////////////////
+
+  ///
+  void init() {}
+
+  ///
+  void client() {}
+
+  ///
+  void sendTx() {}
+
+  ///
+  void signTx() {}
+
+  ///
+  void approve() {}
+
+  ///
+  void swapToken() {}
+
+  ///
+  void transfer() {}
+}
